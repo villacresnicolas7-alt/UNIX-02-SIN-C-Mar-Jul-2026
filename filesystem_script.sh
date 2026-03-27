06e85da16c6bc1bfcac54e4bc1bb572c6a37e572
nicolas <villacresnicolas7@gmail.com>
2026-03-27 15:33:38 +0000

    saludo_bin

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -15,3 +15,14 @@ ls
 #cambiamos a bin
 cd bin
 #vemos los archivos
+ls
+#cambiamos file systems
+cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/saludo
+#vemos los archivos
+ls
+
+gcc saludo.c -o saludo_bin
+
+sudo mv saludo_bin /bin
+
+cd /bin
