d9d13d53a9ba1b9b54b26362b0c601b561058418
nicolas <villacresnicolas7@gmail.com>
2026-03-30 12:54:33 +0000

    Intaling_sistems

---
+++ Parted_intasal.sh
@@ -0,0 +1,6 @@
+#Updates the list of available packages from the repositories.
+sudp apt update
+#Installs the latest available updates for all installed packages.
+sudo apt upgrade
+#Installs the **parted** disk partitioning tool on the system.
+sudo apt install parted
