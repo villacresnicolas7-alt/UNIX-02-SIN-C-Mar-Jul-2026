301890a69e2ceb4ae05fdc2b2d8a8c3320107eb2
nicolas <villacresnicolas7@gmail.com>
2026-03-30 13:09:02 +0000

    CHECK_RECTORY

--- Parted_intasal.sh
+++ Parted_intasal.sh
@@ -4,3 +4,7 @@ sudp apt update
 sudo apt upgrade
 #Installs the **parted** disk partitioning tool on the system.
 sudo apt install parted
+#Shows disk partition details, prints a separator, then lists block devices with filesystem info.
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e
+#Checks if the system uses UEFI or BIOS and prints the result.
+[ -d/sys/firmware/efi ] && echo "UEFI" || echo "BIOS" 
