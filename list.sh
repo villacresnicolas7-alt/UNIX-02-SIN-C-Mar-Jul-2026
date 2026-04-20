03aad7aad14c6f89211b40811cf8b63c35670793
nicolas <villacresnicolas7@gmail.com>
2026-04-20 14:45:51 +0000

    add

--- list.sh
+++ list.sh
@@ -2,3 +2,7 @@ sudo echo "hola" > /etc/archivo_protegido
 #permision denied for the sudo only was ejec in the next line
 echo "hola" | sudo tee /etc/archivo_protegido > /dev/null
 #this command was made three parts, the echo was print like user normaly, th pipe line is the out to the command and the tee was eject like root for the permiss, finally the dev null avoid the contain in the terminal 
+cat /etc/archivo_protegido
+#let me see the archiv absolutly
+sudo sh -c 'echo "chao" >> /etc/archivo_protegido 
+# and a chao to the command exist in the finaly
