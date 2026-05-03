e326e7ab2044e2bbbd7246997c89364243101df4
nicolas <villacresnicolas7@gmail.com>
2026-05-03 19:50:18 +0000

    aptitude_in_repo

--- cisco_documen.sh
+++ cisco_documen.sh
@@ -2,3 +2,6 @@ ls
 #show me the archivents in the directory where i am
 ls Documenst
 #show me the directory documents but here have not this directory so nothing happen 
+aptitude moo 
+#Display aptitude's hidden easter egg
+ls -l
