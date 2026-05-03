7372b30adb02cccf12b053760c6c749c7f292017
nicolas <villacresnicolas7@gmail.com>
2026-05-03 20:43:40 +0000

    password

--- cisco_documen.sh
+++ cisco_documen.sh
@@ -29,5 +29,9 @@ ls -lt /var/log
 #It will sort the files by their timestamp
 ls -l -S /var/log    
 #It will sort for the size of the archivemns 
- ls -lSr /var/log
+ls -lSr /var/log
 #will reverse the order of any type of ordering
+su  -
+#a password fot the virtual machine
+exit
+#leave the terminal
