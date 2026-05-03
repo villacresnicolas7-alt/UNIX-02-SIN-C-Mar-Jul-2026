f66c80bd52ed35b304d4894ae2682becf1284941
nicolas <villacresnicolas7@gmail.com>
2026-05-03 23:34:52 +0000

    Script_linux

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,1 +1,64 @@
-#!/bin/bash
+ls
+#show me the archivents in the directory where i am
+ls Documenst
+#show me the directory documents but here have not this directory so nothing happen 
+aptitude moo 
+#Display aptitude's hidden easter egg
+ls -l
+#show more specifically
+ls -r
+#reverse the result
+ls -l -r
+ls -rl
+#The `-l` option provides a long listing format,
+#and `-r` reverses the listing. 
+#The result of using both options will be a long listing in reverse alphabetical order.
+aptitude -v moo
+#insist in see more easter eggs
+-v 
+#insist more 
+pwd
+#show me in wich directory i am
+cd
+#this comand is for chance the directory 
+cd /
+#we welcome  back in the rame root likewize main
+ls -l /var/log/
+# List detailed log files
+ls -lt /var/log 
+#It will sort the files by their timestamp
+ls -l -S /var/log    
+#It will sort for the size of the archivemns 
+ls -lSr /var/log
+#will reverse the order of any type of ordering
+su  -
+#a password fot the virtual machine
+exit
+#leave the terminal
+sl
+#avoid ejecuted comands privileged
+sudo sl
+#permiss privileged
+# Change to Documents directory
+cd ~/Documents
+# Show detailed file information
+ls -l hello.sh
+# Run script from current directory
+./hello.sh
+# Add execute permission for owner
+chmod u+x hello.sh
+# Show updated file permissions
+ls -l hello.sh
+# Execute the script
+./hello.sh
+# Change file owner to root
+sudo chown root hello.sh
+#show the list in the directry
+cat animals.txt  
+cat alpha.txt    
+# Show first lines of file
+head alpha.txt
+# Show last lines of file
+tail alpha.txt
+# Copy file to current directory
+cp /etc/passwd .
