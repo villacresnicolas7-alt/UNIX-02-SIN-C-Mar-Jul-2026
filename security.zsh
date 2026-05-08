f1fcda6e787075b0dbea64743a73dffd10efae56
nicolas <villacresnicolas7@gmail.com>
2026-05-08 14:38:56 +0000

    comands

--- security.zsh
+++ security.zsh
@@ -1,4 +1,8 @@
 id
+#Displays the current user's UID, GID, and groups
 groups
+#Shows the groups the current user belongs to
 cat /etc/passwd 
+#Displays all system users and account information
 cat /etc/passwd | head -10
+#Shows the first 10 lines of /etc/passwd
