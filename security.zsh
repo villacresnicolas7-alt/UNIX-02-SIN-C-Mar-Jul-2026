8c7d5b05d3cc0cc140083efd9d1593f9a6f1cd1a
nicolas <villacresnicolas7@gmail.com>
2026-05-08 14:42:45 +0000

    next_comands

--- security.zsh
+++ security.zsh
@@ -6,3 +6,7 @@ cat /etc/passwd
 #Displays all system users and account information
 cat /etc/passwd | head -10
 #Shows the first 10 lines of /etc/passwd
+touch test.txt 
+#Creates an empty file named test.txt
+ls -la 
+#Lists all files and directories with detailed information, including hidden files
