32243f8c894728288f8fa30bdd133a133902080f
nicolas <villacresnicolas7@gmail.com>
2026-05-29 15:03:20 +0000

    firts command

---
+++ security2.zsh
@@ -0,0 +1,9 @@
+# See the group main
+id
+id -gn    # only the name of the group main
+# Create the archiv and see the group
+touch ~/test_grupo_heredado.txt
+ls -la ~/test_grupo_heredado.txt
+# is the group main of the user
+groups
+#see the groups 
