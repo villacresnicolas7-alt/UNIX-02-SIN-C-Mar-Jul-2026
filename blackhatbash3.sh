fc5e0e49b21606c45b26c523c9a5353cc0c75e79
nicolas <villacresnicolas7@gmail.com>
2026-06-01 15:42:00 +0000

    printnothing

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 book="black_hat_bash"
 echo "This books name is ${book}"
+echo "This books name is $book"
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+unset book 
+echo "${book}"
