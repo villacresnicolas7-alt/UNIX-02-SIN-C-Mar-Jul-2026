dd737d18251eaaddeea232e6a2ce6204b6a4e34e
nicolas <villacresnicolas7@gmail.com>
2026-06-01 15:25:14 +0000

    depur

---
+++ blackhatbash2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# All this script does is create a directory, create a file
+# within the directory, and then list the contents of the di rectory.
+mkdir mydirectory
+touch mydirectory/myfile
+ls -l mydirectory
