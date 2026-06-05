6f48785152da5bfb7f981ecf978c37c3ddd0d0f7
nicolas <villacresnicolas7@gmail.com>
2026-06-05 15:48:44 +0000

    ground

--- array.sh
+++ array.sh
@@ -11,7 +11,3 @@ chmod u+x array.sh
 
 unset IP_ADDRESSES[1]
 IP_ADDRESSES[0]="192.168.1.10"
-echo "Sleeping for 10 seconds..." 
-sleep 10 & 
-touch test123 
-rm test123
---
+++ backgroup.sh
@@ -0,0 +1,4 @@
+echo "Sleeping for 10 seconds..." 
+sleep 10 & 
+touch test123 
+rm test123
