22bc511f373a2d414f9799167e84168c3bab7b36
nicolas <villacresnicolas7@gmail.com>
2026-06-05 15:32:39 +0000

    cats

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,12 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el lzl commando fallo tio"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "bye  World!" > output.txt
+cat output.txt
+echo "bye  World!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stderr.txt
