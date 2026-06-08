6de45083f83fe8746a63fe0ec8583af21c9b21c1
nicolas <villacresnicolas7@gmail.com>
2026-06-08 15:21:47 +0000

    ips3

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -3,4 +3,8 @@ SCRIPT_NAME="${0}"
 TARGET="${1}" 
 echo "Running the script ${SCRIPT_NAME}..." 
 echo "Pinging the target: ${TARGET}..." 
-ping -c 3"${TARGET}"
+ping -c3 "${TARGET}"
+#bash ping_with_arguments.sh 127.0.0.1
+echo "The arguments are: $@" 
+echo "The total number of arguments is: $#"
+#bash ping_with_arguments.sh 127.0.0.1 8.8.8.8
