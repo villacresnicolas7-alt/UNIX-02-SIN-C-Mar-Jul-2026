d39f438b23d39ada7e043b9add7c2a0b03836091
nicolas <villacresnicolas7@gmail.com>
2026-06-08 15:22:54 +0000

    great

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -8,3 +8,6 @@ ping -c3 "${TARGET}"
 echo "The arguments are: $@" 
 echo "The total number of arguments is: $#"
 #bash ping_with_arguments.sh 127.0.0.1 8.8.8.8
+for args in "$@"; do 
+    echo "${args}" 
+done
