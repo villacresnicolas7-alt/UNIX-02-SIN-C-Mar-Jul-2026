799e06263506b56cfb04ebd3c09c39f9ffc1c415
nicolas <villacresnicolas7@gmail.com>
2026-06-08 15:40:44 +0000

    shmod

--- exit_codes.sh
+++ exit_codes.sh
@@ -3,3 +3,5 @@ ls -l > /dev/null
 echo "The exit code of the ls command was: $?" 
 lzl 2> /dev/null 
 echo "The exit code of the non-existing lzl command was: $?"
+echo "Exiting with exit code: 223"
+exit 223
