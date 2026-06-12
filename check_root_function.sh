487c67438ab7c523b6ed98fef5b3c887fc0ef347
nicolas <villacresnicolas7@gmail.com>
2026-06-12 14:43:24 +0000

    su

---
+++ check_root_function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This function checks if the current user ID equals zero.
+check_if_root() {
+    if [[ "${EUID}" -eq 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+if check_if_root; then
+    echo "User is root!"
+else
+    echo "User is not root!"
+fi
