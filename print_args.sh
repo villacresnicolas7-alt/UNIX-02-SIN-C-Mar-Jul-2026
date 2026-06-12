703cd1604dedc9e8b7345caca45b70ab56239270
nicolas <villacresnicolas7@gmail.com>
2026-06-12 14:49:22 +0000

    Print_FUNCION

---
+++ print_args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Function to print three arguments
+print_args() {
+    echo "first: ${1}, second: ${2}, third: ${3}"
+}
+
+# Call the function with three arguments
+print_args No Starch Press
