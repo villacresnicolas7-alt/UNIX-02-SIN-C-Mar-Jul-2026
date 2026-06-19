3312a88172114e7ba9a8ce9c4e28e74e489ea46f
nicolas <villacresnicolas7@gmail.com>
2026-06-19 15:44:05 +0000

    Godzilla

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -32,3 +32,7 @@ awk 'NR < 10' text.csv
 grep "42.236.10.117" log.txt
 awk '{print $7}'
 grep "42.236.10.117" tex.csv | awk '{print $7}'
+sed 's/Mozilla/Godzilla/g' log.txt
+grep "Godzilla" log.txt
+grep "Mozilla" log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
