ba9fd14791cf0f78521b651acde205ae84575194
nicolas <villacresnicolas7@gmail.com>
2026-06-19 15:10:13 +0000

    greps

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -7,3 +7,9 @@ for ip_address in "$@"; do
   echo "Taking some action on IP address ${ip_address}"
 done
 touch example_file1 example_file2 example_file3
+
+
+grep "35.237.4.214" log.txt
+grep "35.237.4.214\|13.66.139.0" log.txt
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+
